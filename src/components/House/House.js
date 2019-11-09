995a231ca77b9d67e20ce9880edc62a35c6abcfa
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-08 19:21:54 -0700

    second push

--- src/components/House/House.js
+++ src/components/House/House.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react'
 
 
+
 class House extends Component{
     constructor(){
         super()
-        
-
     }
     render(){
         return(
             <div>
+                
+                 <button className="house-delete" type="delete">Delete</button>
                  
             </div>
         )
