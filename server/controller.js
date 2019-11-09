bba23c0f9c18a65ab96d839b7eb3b7bc82d614b9
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-09 11:48:04 -0700

    changes 11-9

--- server/controller.js
+++ server/controller.js
@@ -1,12 +1,12 @@
-module.exports = {
-    getHouses(req, res) {
-        const db = req.app.get('db')
-        db.get_houses()
-        .then(result => {
-            res.satus(200).send(result)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
-}
+// module.exports = {
+//     getHouses(req, res) {
+//         const db = req.app.get('db')
+//         db.get_houses()
+//         .then(result => {
+//             res.satus(200).send(result)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+//     }
+// }
