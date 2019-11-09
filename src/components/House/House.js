0abccadefa00006913114b48afa27962d13acf7c
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-08 17:52:27 -0700

    first push

---
+++ src/components/House/House.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+
+class House extends Component{
+    constructor(){
+        super()
+        
+
+    }
+    render(){
+        return(
+            <div>
+                 
+            </div>
+        )
+    }
+}
+export default House
