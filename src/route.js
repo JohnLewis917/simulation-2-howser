85f4c8974a03a294f9c426edee99fdedc38e8383
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-13 20:14:49 -0700

    changes 11-13

--- src/route.js
+++ src/route.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
 import Wizard from './components/Wizard/Wizard'
+import StepOne from './components/stepOne'
 
 export default(
 
         <Switch>
             <Route exact path='/' component={Dashboard}/>
             <Route path='/wizard' component={Wizard}/>
+            {/* <Route exact path='/wizard/stepOne' component={StepOne}/> */}
         </Switch>
     
 )
