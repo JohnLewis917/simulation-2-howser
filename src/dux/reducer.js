714cfedf65c3db5d6d88f7b6664596634347dd5d
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-13 20:13:58 -0700

    changes 11-13

---
+++ src/dux/reducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+  name: "",
+  address: "",
+  city: "",
+  state: "",
+  zip: 0,
+  image: "",
+  
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        
+    
+        default:
+            return state
+            break;
+    }
+}
