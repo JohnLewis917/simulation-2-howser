a9008284ac1e7678ca721961337551f8adc04e6d
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-14 19:46:07 -0700

    done

--- src/components/stepOne.js
+++ src/components/stepOne.js
@@ -1,42 +1,35 @@
 import React, {Component} from 'react'
 import axios from 'axios'
 import {connect} from 'react-redux'
+import {stepOne} from '../dux/reducer'
 
 
 class StepOne extends Component{
     constructor(){
         super()
-        this.state = {
-            name: '',
-            address: '',
-            city: '',
-            state: '',
-            zip: 0
-        }
-        this.handleName = this.handleName.bind(this)
-        this.handleAddress = this.handleAddress.bind(this)
-        this.handleCity = this.handleCity.bind(this)
-        this.handleState = this.handleState.bind(this)
-        this.handleZip = this.handleZip.bind(this)
-    }
-    handleName(event){
-        const n = this.props.name
         
-        this.setState({name: event.target.value})
+        // this.handleName = this.handleName.bind(this)
+        // this.handleAddress = this.handleAddress.bind(this)
+        // this.handleCity = this.handleCity.bind(this)
+        // this.handleState = this.handleState.bind(this)
+        // this.handleZip = this.handleZip.bind(this)
     }
-    handleAddress(event){
-        this.setState({address: event.target.value})
-    }
-    handleCity(event){
+    // handleName(event){
+    //     this.setState({name: event.target.value})
+    // }
+    // handleAddress(event){
+    //     this.setState({address: event.target.value})
+    // }
+    // handleCity(event){
         
-        this.setState({city: event.target.value})
-    }
-    handleState(event){
-        this.setState({state: event.target.value})
-    }
-    handleZip(event){
-        this.setState({zip: event.target.value})
-    }
+    //     this.setState({city: event.target.value})
+    // }
+    // handleState(event){
+    //     this.setState({state: event.target.value})
+    // }
+    // handleZip(event){
+    //     this.setState({zip: event.target.value})
+    // }
     redirectToTarget = () => {
         this.props.history.push(`/Wizard/stepTwo`)
     }
@@ -48,19 +41,19 @@ class StepOne extends Component{
                 
                 <form>
                     <h5>Property Name</h5>
-                    <input className="input-1" type="text" size="35"  onChange={(e) => this.handleName(e)}></input>
+                    <input className="input-1" name="name" type="text" size="35"  onChange={(e) => this.props.stepOne(e)}></input>
                     <br></br>
                     <h5>Address</h5>
-                    <input className="input-2" type="text" size="35" onChange={(e) => this.handleAddress(e)}></input>
+                    <input className="input-2" name="address" type="text" size="35" onChange={(e) => this.props.stepOne(e)}></input>
                     <br></br>
                     <h5>City</h5>
-                    <input className="input-3" type="text" size="30"  onChange={(e) => this.handleCity(e)}></input>
+                    <input className="input-3" name="city" type="text" size="30"  onChange={(e) => this.props.stepOne(e)}></input>
                     <br></br>
                     <h5>State</h5>
-                    <input className="input-4" type="text" size="5"  onChange={(e) => this.handleState(e)}></input>
+                    <input className="input-4" name="state" type="text" size="5"  onChange={(e) => this.props.stepOne(e)}></input>
                     <br></br>
                     <h5>Zip Code</h5>
-                    <input className="input-5" type="number" size="10" onChange={(e) => this.handleZip(e)}></input>
+                    <input className="input-5" name="zip" type="number" size="10" onChange={(e) => this.props.stepOne(e)}></input>
                     <br></br>
                     <br></br>
                     <button color="primary" className="submitHouse" type="submit" onClick={this.redirectToTarget}>Next Step</button>
@@ -78,4 +71,4 @@ const mapStateToProps = (state) =>{
             state: state.state,
             zip_code: state.zip_code}
 }
-export default connect(mapStateToProps)(StepOne)
+export default connect(mapStateToProps, {stepOne})(StepOne)
