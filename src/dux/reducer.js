497ae61803c322e3847cadb0c5b6baa0ba9d7a90
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-14 19:45:32 -0700

    done

--- src/dux/reducer.js
+++ src/dux/reducer.js
@@ -7,16 +7,49 @@ const initialState = {
   image: "",
   mortgage: 0,
   rent: 0
+};
 
-  
+export const STEP_ONE = "STEP_ONE";
+export const STEP_TWO = "STEP_TWO";
+export const STEP_THREE = "STEP_THREE";
+
+export function stepOne(e) {
+  const { name, value } = e.target;
+  return {
+    type: STEP_ONE,
+    payload: value,
+    name: name
+  };
+}
+export function stepTwo(e) {
+  const { name, value } = e.target;
+  return {
+    type: STEP_TWO,
+    payload: value,
+    name: name
+  };
+
+}
+export function stepThree(e) {
+  const { name, value } = e.target;
+  return {
+    type: STEP_THREE,
+    payload: value,
+    name: name
+  };
 }
 
 export default function reducer(state = initialState, action) {
-    switch (action.type) {
-        
-    
-        default:
-            return state
-            break;
-    }
+  console.log(action);
+  switch (action.type) {
+    case STEP_ONE:
+      return { ...state, [action.name]: action.payload };
+    case STEP_TWO:
+      return { ...state, [action.name]: action.payload };
+    case STEP_THREE:
+      return { ...state, [action.name]: action.payload };
+    default:
+      return state;
+      break;
+  }
 }
