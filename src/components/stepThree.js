799202136d1bc87a2f34900291bc5e9a57ad8102
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-14 19:46:00 -0700

    done

--- src/components/stepThree.js
+++ src/components/stepThree.js
@@ -1,33 +1,35 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import {connect} from 'react-redux'
+import {stepThree} from '../dux/reducer'
 
 
 class StepThree extends Component{
     constructor(){
         super()
-        this.state = {
-            name: '',
-            address: '',
-            city: '',
-            state: '',
-            zip: 0
-        }
-        this.handleMortgage = this.handleMortgage.bind(this)
-        this.handleRent = this.handleRent.bind(this)
+        // this.state = {
+        //     name: '',
+        //     address: '',
+        //     city: '',
+        //     state: '',
+        //     zip: 0
+        // }
+    //     this.handleMortgage = this.handleMortgage.bind(this)
+    //     this.handleRent = this.handleRent.bind(this)
         
-    }
-    handleMortgage(event){
-        this.setState({name: event.target.value})
-    }
-    handleRent(event){
-        this.setState({address: event.target.value})
+    // }
+    // handleMortgage(event){
+    //     this.setState({name: event.target.value})
+    // }
+    // handleRent(event){
+    //     this.setState({address: event.target.value})
     }
     redirectToTargetPrevious = () => {
         this.props.history.push(`/Wizard/stepTwo`)
     }
     
     addHouse(){
-        axios.post('/api/Dashboard', this.state)
+        axios.post('/api/Dashboard', this.props)
         .then(res => {
         this.props.history.push('/')
         })
@@ -39,10 +41,10 @@ class StepThree extends Component{
                 
                 <form>
                     <h5>Monthly Mortgage Amount</h5>
-                    <input className="input-1" type="text" size="35"  onChange={(e) => this.handleMortgage(e)}></input>
+                    <input className="input-1" name="mortgage" type="text" size="35"  onChange={(e) => this.props.stepThree(e)}></input>
                     <br></br>
                     <h5>Desired Monthly Rent</h5>
-                    <input className="input-2" type="text" size="35" onChange={(e) => this.handleRent(e)}></input>
+                    <input className="input-2" name="rent" type="text" size="35" onChange={(e) => this.props.stepThree(e)}></input>
                     <br></br>
                     <br></br>
                     <button color="primary" className="submitHouse" type="submit" onClick={this.redirectToTargetPrevious}>Previous Step</button>
@@ -54,4 +56,8 @@ class StepThree extends Component{
         )
     }
 }
-export default StepThree 
+
+function mapStateToProps(state){
+    return state
+}
+export default connect (mapStateToProps, {stepThree})(StepThree)
