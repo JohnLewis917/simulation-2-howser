978f3faa4c8c8d338e6d714456cc6d1ce53d1a81
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-14 19:46:20 -0700

    done

--- db/add_house.sql
+++ db/add_house.sql
@@ -1,2 +1,2 @@
-insert into houses (name, address, city, state, zip_code)
-values(${name}, ${address}, ${city},${state},${zip_code});
+insert into houses (name, address, city, state, zip_code, image, mortgage, rent)
+values(${name}, ${address}, ${city},${state},${zip_code}, ${image}, ${mortgage}, ${rent});
